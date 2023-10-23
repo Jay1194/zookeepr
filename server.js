64009c7128030c39daacdb54d71f45e961fe68b8
Jay <j.thomastt1@gmail.com>
2023-10-23 10:21:54 -0600

    set up server

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 const express = require('express');
 
-// Setting up the server only takes two steps: we need to instantiate the server, then tell it to listen for requests. To instantiate the server
+// instantiate the server
 const app = express();
 
-// tell it to listen for requests
+// tell the server to listen for requests
 app.listen(3001, () => {
     console.log(`API server now on port 3001!`);
 });
